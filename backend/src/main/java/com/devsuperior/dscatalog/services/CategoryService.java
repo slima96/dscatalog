5bba1f622d21843c9e8f5d0858dd3febccf8185e
Sandro Lima <sandro.lima0016@gmail.com>
2022-03-26 18:55:22 -0300

    POST new category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -33,4 +33,13 @@ public class CategoryService {
 		
 		return new CategoryDTO(entity);
 	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity =  new Category();
+		entity.setName(dto.getName());
+		entity = repo.save(entity);
+		
+		return new CategoryDTO(entity);
+	}
 }
